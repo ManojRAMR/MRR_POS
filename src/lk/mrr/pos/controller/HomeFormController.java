7093aee5619d79f1b8a10109a2ac0dc3b368ad4f
Manoj R. Randeni <randeni.ramr@gmail.com>
2021-05-02 17:28:23 +0530

    Define btnCustomerOnClick method

--- src/lk/mrr/pos/controller/HomeFormController.java
+++ src/lk/mrr/pos/controller/HomeFormController.java
@@ -1,4 +1,34 @@
 package lk.mrr.pos.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class HomeFormController {
+    public ImageView btnCustomer;
+    public ImageView btnItem;
+    public ImageView btnCart;
+    public ImageView btnOrderList;
+
+    public void btnCustomerOnClick(MouseEvent mouseEvent) throws IOException {
+        URL resource = getClass().getResource("lk/mrr/pos/view/CustomerForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Scene scene = new Scene(load);
+
+    }
+
+    public void btnItemOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void btnCartOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void btnOrderListOnClick(MouseEvent mouseEvent) {
+    }
 }
